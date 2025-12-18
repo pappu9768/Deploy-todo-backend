4b1cc0243fc75ad800e4141d3583d0ffbb18e625
Pawan <9930pawanshah@gmail.com>
2025-12-19 01:08:45 +0530

    heeellloo

--- src/DB/connectDb.js
+++ src/DB/connectDb.js
@@ -2,18 +2,21 @@ import mongoose from "mongoose";
 
 
 
-const connectDB = async () => {
-  
+const connectDB = async (req, res) => {
 
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
 
-    
-    console.log(`MongoDB connected : ${conn.connection.host}`);
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    throw error;
-  }
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+
+
+        console.log(`MongoDB connected : ${conn.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        return res.status(400).json({
+            message:"Error found",
+            error
+        })
+    }
 };
 
 export default connectDB;
