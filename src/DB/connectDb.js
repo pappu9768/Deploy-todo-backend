6e2a62094587b80288dd8e7e67ba21cc079ee1d4
Pawan <9930pawanshah@gmail.com>
2025-12-19 00:56:18 +0530

    AGAIN

--- src/DB/connectDb.js
+++ src/DB/connectDb.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
+
 
 const connectDB = async () => {
-  if (isConnected) return;
+  
 
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
 
-    isConnected = true;
+    
     console.log(`MongoDB connected : ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error);
