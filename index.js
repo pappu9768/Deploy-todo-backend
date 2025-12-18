73ea6593e84d2b15c9c1bd0db7d20abb3335979b
Pawan <9930pawanshah@gmail.com>
2025-12-19 00:39:19 +0530

    second

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import connectDB from './src/DB/connectDb.js';
 import routes from './src/routes/user.routes.js';
 dotenv.config();
 import cors from 'cors'
@@ -9,17 +9,17 @@ import { userAuthorization } from './src/middlewares/userAuthorization.js';
 
 const app = express();
 
-let isConnected = false
-async function connectDB(){
-    try {
-        const mongoUri = process.env.MONGO_URI
-        const con = await mongoose.connect(mongoUri)
-        isConnected = true
-        console.log(`Database Connected successfully ${con.connection.host}`)
-    } catch (error) {
-        console.log(error)
-    }
-}
+// let isConnected = false
+// async function connectDB(){
+//     try {
+//         const mongoUri = process.env.MONGO_URI
+//         const con = await mongoose.connect(mongoUri)
+//         isConnected = true
+//         console.log(`Database Connected successfully ${con.connection.host}`)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
 
 app.use(express.json());
@@ -27,16 +27,21 @@ app.use(cors({
     origin: 'http://localhost:5173'
 }))
 
-app.use((req,res,next) => {
-    if(!isConnected){
-        connectDB()
-    }
-    next();
-})
+// app.use((req,res,next) => {
+//     if(!isConnected){
+//         connectDB()
+//     }
+//     next();
+// })
 // routes 
 app.use('/api/v1/auth', routes);
 
-app.get('/api/loggedIn',userAuthorization, (req, res) => {
+app.get("/", async (req, res) => {
+    await connectDB();
+    res.send("Backend running");
+});
+
+app.get('/api/loggedIn', userAuthorization, (req, res) => {
     try {
         res.status(200).json({
             success: true,
--- src/DB/connectDb.js
+++ src/DB/connectDb.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
-let isConnected = false
-const connectDB = async() => {
-    try {
-        const mongoUri = process.env.MONGO_URI
-        const con = await mongoose.connect(mongoUri)
-        isConnected = true
-        console.log(`Database Connected successfully ${con.connection.host}`)
-    } catch (error) {
-        console.log(error)
-    }
-}
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+
+    isConnected = true;
+    console.log(`MongoDB connected : ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
+};
 
 export default connectDB;
